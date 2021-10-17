33b0c228dd0bf743fc5b79c9f7593c9e97875dbb
Thomas Teague <thomas.d.teague1@gmail.com>
2021-10-17 11:39:20 -0400

    Refactored the ETL pipeline classes
    I implemented a structure that standardizes the inputs we provide to the
    functions within the ETL pipeline factory classes. I also provided
    documentation for all classes and functions.

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,6 +1,8 @@
 """Module to construct an ETL pipeline."""
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Callable, Dict
 
 import pandas as pd
 
@@ -10,70 +12,129 @@ from components.transformer import transform_care_dataframe
 from components.transformer import transform_hospital_dataframe
 from components.loader import Loader, HospitalDataLoader
 
+
+@dataclass
+class ExtractorInput:
+    """Class we pass to the methods of ETLPipeline instantiations"""
+    file_dict: Dict[str, Path]
+
+
+@dataclass
+class TransformerInput:
+    """Class we pass to the methods of ETLPipeline instantiations"""
+
+    data_dict: Dict[str, pd.DataFrame]
+    transform_func_dict: Dict[str, Callable[[pd.DataFrame], pd.DataFrame]]
+
+
+@dataclass
+class LoaderInput:
+    """Class we pass to the methods of ETLPipeline instantiations"""
+
+    transformed_data_dict: Dict[str, pd.DataFrame]
+    database_name_stem: str
+    database_subdir: str
+
+
 class ETLPipeline(ABC):
+    """Abstract class that implements a factory for ETL pipeline components."""
 
     @abstractmethod
-    def __init__(self, **kwargs) -> None:
-        pass
+    def get_extractor(self, extractor_input: ExtractorInput) -> Extractor:
+        """Retrieve a class that performs data extraction."""
 
     @abstractmethod
-    def get_extractor(self) -> Extractor:
-        pass
+    def get_transformer(
+        self, transformer_input: TransformerInput) -> DataTransformer:
+        """Retrieve a class that performs data transformation."""
 
     @abstractmethod
-    def get_transformer(self) -> DataTransformer:
-        pass
+    def get_loader(self, loader_input: LoaderInput) -> Loader:
+        """Retrieve a class that creates a storage layer."""
 
-    @abstractmethod
-    def get_loader(self) -> Loader:
-        pass
 
 class HospitalETLPipeline(ETLPipeline):
+    """Class that creates an ETL pipeline specific to data about hospitals."""
 
-    def __init__(self, **kwargs) -> None:
-        pass
+    def get_extractor(
+        self, extractor_input: ExtractorInput) -> Extractor:
+        """
+        Return an object to extract the hospital data.
 
-    def get_extractor(self) -> Extractor:
-        file_dict = {
-            "hospital": Path.cwd().joinpath(
-                "data", "Hospital_General_Information.csv"),
-                "care": Path.cwd().joinpath(
-                    "data", "Timely_and_Effective_Care-Hospital.csv")
-        }
+        :param extractor_input: Input parameters necessary to
+            construct an object that extracts the hospital data from
+            a local directory
+        :type extractor_input: ExtractorInput
+        :return: An object to perform data extraction with the
+            hospital data
+        :rtype: Extractor
+        """
+        return LocalExtractor(file_dict=extractor_input.file_dict)
 
-        return LocalExtractor(file_dict=file_dict)
+    def get_transformer(
+        self, transformer_input: TransformerInput) -> DataTransformer:
+        """
+        Return an object to transform the hospital data.
 
-    def get_transformer(self, **kwargs) -> DataTransformer:
-        data_dict = kwargs["data_dict"]
-        transform_func_dict = {
-            "care": transform_care_dataframe,
-            "hospital": transform_hospital_dataframe
-        }
+        :param transformer_input: Input parameters necessary to
+            construct an object that transforms the hospital data
+        :type transformer_input: TransformerInput
+        :return: An object to perform data transformation with the
+            hospital data
+        :rtype: DataTransformer
+        """
+        data_dict = transformer_input.data_dict
+        transform_func_dict = transformer_input.transform_func_dict
 
         return CSVDataTransformer(
             data_dict=data_dict, transform_func_dict=transform_func_dict)
 
-    def get_loader(self, **kwargs) -> Loader:
+    def get_loader(self, loader_input: LoaderInput) -> Loader:
+        """
+        Return an object to place transformed hospital data in a storage layer.
+
+        :param loader_input: Input parameters necessary to
+            construct an object that loads the transformed hospital
+            data into a storage layer
+        :type loader_input: LoaderInput
+        :return: An object to load the transformed hospital data into
+            a storage layer
+        :rtype: Loader
+        """
         return HospitalDataLoader(
-            transformed_data_dict=kwargs["transformed_data_dict"],
-            database_name_stem=kwargs["database_name_stem"],
-            database_subdir=kwargs["database_subdir"])
+            transformed_data_dict=loader_input.transformed_data_dict,
+            database_name_stem=loader_input.database_name_stem,
+            database_subdir=loader_input.database_subdir)
 
 
 def main(
         database_name_stem: str,
         database_subdir: str) -> None:
+    """Build an ETL pipeline and create a database for hospital data."""
     pipeline = HospitalETLPipeline()
-    extractor = pipeline.get_extractor()
+    file_dict  = {
+        "hospital": Path.cwd().joinpath(
+            "data", "Hospital_General_Information.csv"),
+        "care": Path.cwd().joinpath(
+            "data", "Timely_and_Effective_Care-Hospital.csv")
+    }
+    extractor = pipeline.get_extractor(ExtractorInput(file_dict=file_dict))
     data_dict = extractor.load_data()
 
-    transformer = pipeline.get_transformer(data_dict=data_dict)
+    transform_func_dict = {
+        "care": transform_care_dataframe,
+        "hospital": transform_hospital_dataframe
+    }
+    transformer = pipeline.get_transformer(
+        TransformerInput(
+            data_dict=data_dict, transform_func_dict=transform_func_dict))
     transformed_data_dict = transformer.transform_data()
 
     loader = pipeline.get_loader(
-        transformed_data_dict=transformed_data_dict,
-        database_name_stem=database_name_stem,
-        database_subdir=database_subdir)
+        HospitalDataLoader(
+            transformed_data_dict=transformed_data_dict,
+            database_name_stem=database_name_stem,
+            database_subdir=database_subdir))
     loader.create_storage_layer()
 
 
